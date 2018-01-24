16e6246ac78d08afa801368445dba5c57393b76a
David Szöke <david.szoeke@unic.com>
2018-01-24 20:22:46 +0100

    Remove support assemblies since the caching issues seem to have been fixed in 9.0 initial release

--- NitroNet.Sitecore/MvcRendering/Pipelines/GenerateCacheKey.cs
+++ NitroNet.Sitecore/MvcRendering/Pipelines/GenerateCacheKey.cs
@@ -1,6 +1,6 @@
 ﻿namespace NitroNet.Sitecore.MvcRendering.Pipelines
 {
-    public class GenerateCacheKey : global::Sitecore.Support.Mvc.Pipelines.Response.RenderRendering.GenerateCacheKey
+    public class GenerateCacheKey : global::Sitecore.Mvc.Pipelines.Response.RenderRendering.GenerateCacheKey
     {
         protected override string GetDataPart(global::Sitecore.Mvc.Presentation.Rendering rendering)
         {
